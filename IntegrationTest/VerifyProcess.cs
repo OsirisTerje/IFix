3f21ed8298ea0cf0224ad2f28b603e5eaca0c410
Terje Sandstrom <terje@hermit.no>
2014-05-29 00:50:24 +0200

    integration test for remove nuget exe

--- IntegrationTest/VerifyProcess.cs
+++ IntegrationTest/VerifyProcess.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using CleanCA0053Cmd;
@@ -11,12 +13,26 @@ namespace IntegrationTest
     [TestFixture]
     public class VerifyProcess
     {
+        private const string subpath = "/../../../TestProject/TestProject";
         [Test]
         public void CheckDeletingNugetExe()
         {
             var v = new RemoveOldNugetRestore();
-            
+            string path = Directory.GetCurrentDirectory() + subpath ;
+            string fileisat = path + "/.nuget/nuget.exe";
+            bool exist = File.Exists(fileisat);
+            Assert.IsTrue(exist);
+            v.RemoveAllNugetExeFiles(path);
+            exist = File.Exists(fileisat);
+            Assert.IsFalse(exist);
+        }
 
+        [Test]
+        public void VerifyCorrectDirectory()
+        {
+            string path = Directory.GetCurrentDirectory() + subpath;
+            var exist = File.Exists(path + "/TestProject.csproj");
+            Assert.IsTrue(exist);
         }
 
     }
