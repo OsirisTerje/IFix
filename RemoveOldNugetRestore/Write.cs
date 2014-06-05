bbf8ad00a0ec4581a0fba628f117dd8bec08ac05
Terje Sandstrom <terje@hermit.no>
2014-06-06 00:34:59 +0200

    added check for gitignore

---
+++ RemoveOldNugetRestore/Write.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFix
+{
+    public static class Writer
+    {
+        public static void WriteRed(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void WriteGreen(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Write(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+    }
+}
