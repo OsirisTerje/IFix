7b6fc33a6386d43a639915ac87785e3bab4ad9de
Terje Sandstrom <terje@hermit.no>
2014-06-07 23:49:13 +0200

    added missing files

---
+++ GitIgnoreTests/ResourceReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIgnoreTests
+{
+    public static class ResourceReader
+    {
+        public static List<string> Read(string resourceName)
+        {
+            var resource =
+                Assembly.GetExecutingAssembly().GetManifestResourceNames()
+                    .FirstOrDefault(item => item.ToUpper().EndsWith(resourceName.ToUpper()));
+            if (resource==null)
+                throw new ArgumentException("No test file resource named : " + resourceName);
+            string content = "";
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    content= reader.ReadToEnd();
+                }
+            }
+            var lines = content.Split(new[] {"\r\n", "\n"}, StringSplitOptions.None);
+            return new List<string>(lines);
+
+
+        }
+    }
+}
