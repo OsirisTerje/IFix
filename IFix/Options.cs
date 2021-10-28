3a7b84e40b31f815e84de4da797a5b1fbe350312
Terje Sandstrøm <terje@hermit.no>
2021-10-29 00:52:30 +0200

    Updated to be a dotnet tool, running under net 5

--- IFix/Options.cs
+++ IFix/Options.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using CommandLine;
 
 namespace IFix
 {
@@ -7,14 +8,14 @@ namespace IFix
 
     public abstract class Options
     {
-        // [Option('c', "check", HelpText = "Only check if we have an issue. No changes done")]
+        [Option('c', "check", HelpText = "Only check if we have an issue. No changes done")]
         public bool Check { get; set; }
 
 
-        // [Option('f', "fix", HelpText = "Fix all files with issues")]
+         [Option('f', "fix", HelpText = "Fix all files with issues")]
         public bool Fix { get; set; }
 
-        // [Option('v', "verbose", HelpText = "More verbose output")]
+        [Option('v', "verbose", HelpText = "More verbose output")]
         public bool Verbose { get; set; }
 
         // [HelpOption]
@@ -36,6 +37,7 @@ namespace IFix
         }
     }
 
+    [Verb("info")]
     public class GoToBlog : Options
     {
         private const string UrlIFix = @"https://visualstudiogallery.msdn.microsoft.com/b8ba97b0-bb89-4c21-a1e2-53ef335fd9cb";
@@ -63,31 +65,25 @@ namespace IFix
             else if (CreateSln)
                 url = UrlCreateSln;
 
-            using (var b = new Process
-            {
-                StartInfo =
-                    new ProcessStartInfo(url)
-            })
-            {
-                b.Start();
-                return 0;
-            }
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            return 0;
         }
 
-        //[Option('g', "gitignore", HelpText = "gitignore blog")]
+        [Option('g', "gitignore", HelpText = "gitignore blog")]
         public bool GitIgnore { get; set; }
-        //[Option('n', "nugetrestore", HelpText = "nugetrestore blog")]
+
+        [Option('n', "nugetrestore", HelpText = "nugetrestore blog")]
         public bool NuGetRestore { get; set; }
-        //[Option('a', "ca0053", HelpText = "ca0053 blog")]
+        [Option('a', "ca0053", HelpText = "ca0053 blog")]
         public bool Ca0053 { get; set; }
-
+        [Option('e', "mefcache")]
         public bool MefCache { get; set; }
-
+        [Option('s', "createsln", HelpText = "Creates a blank solution file")]
         public bool CreateSln { get; set; }
 
     }
 
-    //[Verb("nugetrestore")]
+    [Verb("nugetrestore")]
     public class NuGetRestoreCommand : Options
     {
         public override int Execute()
@@ -97,6 +93,7 @@ namespace IFix
         }
     }
 
+    [Verb("fixCa0053")]
     public class FixCA0053Command : Options
     {
         public override int Execute()
@@ -106,7 +103,7 @@ namespace IFix
         }
     }
 
-
+    [Verb("CreateSln")]
     public class CreateSln : Options
     {
         public override int Execute()
@@ -115,13 +112,14 @@ namespace IFix
             return fixer.Execute(this);
         }
 
-        //[ValueOption(0)]
+        [Value(0)]
         public string Name { get; set; }
 
-        //[Option('b',"blank",HelpText="Create a complete blank solution")]
+        [Option('b',"blank",HelpText="Create a complete blank solution")]
         public bool Blank { get; set; }
     }
 
+    [Verb("MefCache")]
     public class MefCacheCommand : Options
     {
         public override int Execute()
@@ -129,17 +127,17 @@ namespace IFix
             var fixer = new DeleteMefCache();
             return fixer.Execute(this);
         }
-        //[Option('2',"vs2012",HelpText="Delete cache for VS2012")]
+        [Option('2',"vs2012",HelpText="Delete cache for VS2012")]
         public bool Vs2012 { get; set; }
-        //[Option('3', "vs2013", HelpText = "Delete cache for VS2013")]
+        [Option('3', "vs2013", HelpText = "Delete cache for VS2013")]
         public bool Vs2013 { get; set; }
-        //[Option('5', "vs2015", HelpText = "Delete cache for VS2015")]
+        [Option('5', "vs2015", HelpText = "Delete cache for VS2015")]
         public bool Vs2015 { get; set; }
 
-        //[Option('7', "vs2017", HelpText = "Delete cache for VS2017")]
+        [Option('7', "vs2017", HelpText = "Delete cache for VS2017")]
         public bool Vs2017 { get; set; }
 
-        //[Option('a', "all", HelpText = "Delete cache for all VS instances")]
+        [Option('a', "all", HelpText = "Delete cache for all VS instances")]
         public bool All { get; set; }
 
         public bool NotSpecific => !Vs2012 && !Vs2013 && !Vs2015;
@@ -152,7 +150,7 @@ namespace IFix
         //}
     }
 
-
+    [Verb("VSTestcache")]
     public class VSTestCacheCommands : Options
     {
         public override int Execute()
@@ -162,22 +160,22 @@ namespace IFix
         }
     }
 
-
+    [Verb("gitignore")]
     public class GitIgnoreCommand : Options
     {
-        //[Option('a', "add", HelpText = "Only add latest standard public .gitignore when missing, don't fix up the others.")]
+        [Option('a', "add", HelpText = "Only add latest standard public .gitignore when missing, don't fix up the others.")]
         public bool Add { get; set; }
 
-        //[Option('m', "merge", HelpText = "Get and merge information from the  standard public gitignore file")]
+        [Option('m', "merge", HelpText = "Get and merge information from the  standard public gitignore file")]
         public bool Merge { get; set; }
 
-        //[Option('r', "replace", HelpText = "Replace the existing instead of merging in the latest, applies to all gitignore files")]
+        [Option('r', "replace", HelpText = "Replace the existing instead of merging in the latest, applies to all gitignore files")]
         public bool Replace { get; set; }
 
-        //[Option('l',"latestgit",HelpText = "Use for compatibility with the latest git version 2.0.1. If you have git < 2.0.1 leave this out.")]
+        [Option('l',"latestgit",HelpText = "Use for compatibility with the latest git version 2.0.1. If you have git < 2.0.1 leave this out.")]
         public bool LatestGitVersion { get; set; } = true;
 
-        //[Option('s',"strict",HelpText="Ensure that the most extensive pattern is used")]
+        [Option('s',"strict",HelpText="Ensure that the most extensive pattern is used")]
         public bool Strict { get; set; }
 
         //[HelpOption]
@@ -207,46 +205,48 @@ namespace IFix
         }
     }
 
+    [Verb("NugetConsolidate")]
     public class NugetConsolidateCommands : Options
     {
 
 
-        //[Option('s', "short", HelpText = "Only list packages and versions, skip the project information")]
+        [Option('s', "short", HelpText = "Only list packages and versions, skip the project information")]
         public bool Short { get; set; }
 
-        //[Option('o', "consolidate", HelpText = "Only list packages that needs consolidation")]
+        [Option('o', "consolidate", HelpText = "Only list packages that needs consolidation")]
         public bool Consolidate { get; set; }
 
-
-
+        [Option('p', "packageconfig", HelpText = "Use for when you have package.config based projects")]
+        public bool PackageConfig { get; set; } = false;
         public override int Execute()
         {
-            var fixer = new NugetConsolidate();
+            INugetConsolidate fixer = PackageConfig ? new NugetConsolidateOld() : new NugetConsolidate();
             return fixer.Execute(this);
         }
     }
 
+    [Verb("Diagnostics")]
     public class DiagnosticsCommands : Options
     {
 
-        //[Option('s',"show",HelpText = "Show all diagnostics settings")]
+        [Option('s',"show",HelpText = "Show all diagnostics settings")]
         public bool Show { get; set; }
 
-        //[Option('d', "Dump", DefaultValue = -1, HelpText = "Enable or Disable dump")]
+        [Option('d', "Dump", Default = -1, HelpText = "Enable or Disable dump")]
         public int EnableDisableDump { get; set; }
 
-        //[Option('D', "Dumpfolder", DefaultValue="",HelpText = "Set dumpfolder")]
+        [Option('D', "Dumpfolder", Default = "",HelpText = "Set dumpfolder")]
         public string DumpFolder { get; set; }
 
-        //[Option('u', "Fuslog", DefaultValue = -1, HelpText = "Enable or Disable fuslog")]
+        [Option('u', "Fuslog", Default = -1, HelpText = "Enable or Disable fuslog")]
         public int EnableDisableFuslog { get; set; }
 
-        //[Option('U', "Fuslogfolder", DefaultValue = "", HelpText = "Set fuslog logfolder")]
+        [Option('U', "Fuslogfolder", Default = "", HelpText = "Set fuslog logfolder")]
         public string FuslogFolder { get; set; }
 
 
-        //[Option('t', "Vstest", DefaultValue = -1, HelpText =
-        //"Enable or Disable vstest tracing, 0 = disable, 1 = enable discovery 2= enable execution, 3 = enable both")]
+        [Option('t', "Vstest", Default = -1, HelpText =
+        "Enable or Disable vstest tracing, 0 = disable, 1 = enable discovery 2= enable execution, 3 = enable both")]
         public int VSTestTracing { get; set; }
 
 
